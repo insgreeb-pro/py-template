4cb6b5575672781cc89a6bc1f3ee742694db8182
Muhammad Wafa <wafax.4@gmail.com>
2021-03-01 00:45:23 +0700

    Fix bug: devided by zero

--- helper/processing.py
+++ helper/processing.py
@@ -6,9 +6,12 @@ COMFORT_TYPES = [
 ]
 
 def calculateComfort(output):
-    status_format = '%s (%.2f %)'
+    status_format = '%s (%.2f %%)'
     comfort = output['kenyamanan']
-    precentage = comfort.count(1) / len(comfort)
+    total_prediction = len(comfort)
+    if total_prediction == 0: # zero output data
+        return "Data Kurang"
+    precentage = comfort.count(1) / total_prediction
     rules = [60, 80]
     for i, rule in enumerate(rules):
         if precentage < rule:
