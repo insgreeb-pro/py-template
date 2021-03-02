1d55d3c8eab6bdc379e94a755f802ab66180b6cd
mwafa <wafax.4@gmail.com>
2021-03-02 19:07:07 +0700

    update push command

--- push.bat
+++ push.bat
@@ -1,2 +1,6 @@
-docker build -t localhost:5000/ai-randomforest-v2 .
-docker push localhost:5000/ai-randomforest-v2 
+docker build -t localhost:5000/ai-randomforest .
+docker push localhost:5000/ai-randomforest 
+docker build -t localhost:5000/ai-ann .
+docker push localhost:5000/ai-ann 
+docker build -t localhost:5000/ai-randomforest-ann .
+docker push localhost:5000/ai-randomforest-ann 
