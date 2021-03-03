a80fc1261338203aa85faec850936fe6cfc603f0
Muhammad Wafa <wafax.4@gmail.com>
2021-03-03 12:55:50 +0700

    fixbug

--- helper/processing.py
+++ helper/processing.py
@@ -12,6 +12,7 @@ def calculateComfort(output):
     if total_prediction == 0: # zero output data
         return "Data Kurang"
     precentage = comfort.count(1) / total_prediction
+    precentage *= 100
     rules = [60, 80]
     for i, rule in enumerate(rules):
         if precentage < rule:
